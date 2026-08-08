db2713856b96a19e408e8cefb95b5027d45c9b75
Disha Holmukhe <dishaholmukhe521@gmail.com>
2026-08-08 23:36:35 +0530

    added the source tag in ingestion layer output

--- ingestion_layer.py
+++ ingestion_layer.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+import os
 def file_upload(file_name):
     input_data = {}
     try:
@@ -9,10 +9,7 @@ def file_upload(file_name):
             timing = time_slot.strftime("%Y-%m-%d %H:%M:%S")
             input_data['data'] = text
             input_data['time'] = timing
+            input_data['source_tag'] = os.path.abspath(file_name)
         return input_data
     except FileNotFoundError as e:
         return e
-
-ans = file_upload('text2.txt')
-print(ans)
-
