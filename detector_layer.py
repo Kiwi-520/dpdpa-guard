e322453a2e26416cc6066738f0394ffd00a7b743
Disha Holmukhe <dishaholmukhe521@gmail.com>
2026-08-16 21:35:11 +0530

    adding validation check

--- detector_layer.py
+++ detector_layer.py
@@ -7,6 +7,8 @@ import regex as re
 from recognizers import ALL_RECOGNIZERS
 from ingestion_layer import file_upload
 
+from pprint import pprint
+
 # add it to registry
 registry = RecognizerRegistry()
 registry.load_predefined_recognizers(
@@ -62,17 +64,13 @@ def detector(input_data:dict):
 
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
-
-text = "Dr. John Smith is a Professor at ABC Technologies. His Aadhaar number is 2345-6789-1234 and his PAN card number is ABCDE1234F. His passport number is A1234567 and voter ID is ABC1234567. His driving license number is MH-12-20210012345. The bank IFSC code is SBIN0001234. You can send money to his UPI ID john.smith@oksbi. His registered vehicle number is MH 12 AB 1234. Contact him at john.smith@gmail.com or call him at +91 9876543210. His office IP address is 192.168.1.100."
-
-doc = nlp(text)
-tokens = []
-for token in doc:
-    # print(token.text)
-    tokens.append(token)
-
-def validation(detected_data):
+def boundary_check(file_data, detected_data):
+    # spacy loading
+    nlp = spacy.load("en_core_web_sm")
+    doc = nlp(file_data['data'])
+    tokens = []
+    for token in doc:
+        tokens.append(token)
     # token check
     for each_entity in detected_data['entities']:
         if each_entity['entity'] in tokens:
@@ -81,11 +79,20 @@ def validation(detected_data):
             each_entity['score'] -= 0.2
     return detected_data
 
-data = file_upload('text.txt')
-detected_data = detector(data)
-validated_data = validation(detected_data)
+def valid_check(boundary_check_data):
+    for each_entity in boundary_check_data['entities']:
+        if each_entity['score'] <= 0.5:
+            boundary_check_data['entities'].remove(each_entity)
+    return boundary_check_data
 
-print(validated_data)
+file_content = file_upload('text.txt')
+detected_data = detector(file_content)
+boundary_checked = boundary_check(file_content, detected_data)
+validated_data = valid_check(boundary_checked)
+
+pprint(validated_data, sort_dicts = False)
+
+# print(boundary_checked['entities'])
 
 
 # text = "Dr. John Smith is a Professor at ABC Technologies. His Aadhaar number is 2345-6789-1234 and his PAN card number is ABCDE1234F. His passport number is A1234567 and voter ID is ABC1234567. His driving license number is MH-12-20210012345. The bank IFSC code is SBIN0001234. You can send money to his UPI ID john.smith@oksbi. His registered vehicle number is MH 12 AB 1234. Contact him at john.smith@gmail.com or call him at +91 9876543210. His office IP address is 192.168.1.100."
