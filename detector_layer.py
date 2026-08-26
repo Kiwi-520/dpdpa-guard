dbe92bb0a6543dd1bc1f60f071c5b2aca8d5eb25
Disha Holmukhe <dishaholmukhe521@gmail.com>
2026-08-26 15:58:30 +0530

    implemented validation overlap check

--- detector_layer.py
+++ detector_layer.py
@@ -30,7 +30,7 @@ for i in ALL_RECOGNIZERS:
 
 analyzer = AnalyzerEngine(registry=registry)
 
-# ------ checking recognizers -----
+# # ------ checking recognizers -----
 # print("Name Entity Code")
 # for r in registry.recognizers:
 #     print(
@@ -91,21 +91,84 @@ def boundary_checkor(file_data, detected_data):
         if word['boundary_status'] == "fragment":
             if word['start'] in position_dict and (word['end'] in position_dict.values() or word['end']+1 in position_dict.values()):
                 word['boundary_status'] = "multi-token"
-
+    # # overlapping check
+    # priority_dict = {
+    #     "EMAIL_ADDRESS" : 9,
+    #     "IP_ADDRESS" : 8,
+    #     "IND_ADHAAR" : 7,
+    #     "IND_PAN" : 7,
+    #     "IND_PASSPORT" : 7,
+    #     "IND_VOTER_ID" : 7,
+    #     "IND_IFSC" : 7,
+    #     "IND_UPI_ID" : 7,
+    #     "IND_DRIVING_LICENSE" : 7,
+    #     "IND_VEHICLE_REGISTRATION_NUMBER" : 7,
+    #     "PERSON" : 6,
+    #     "CREDIT_CARD" : 6,
+    #     "PHONE_NUMBER" : 4,
+    # }
+    # to_remove = set()
+    # for i, wordA in enumerate(detected_data['entities']):
+    #     for j, wordB in enumerate(detected_data['entities']):
+    #         if i < j:
+    #             if (wordA['start'] == wordB['start'] and wordA['end'] == wordB['end']) or (wordB['start'] >= wordA['start'] and wordB['start'] < wordA['end']) or (wordA['start'] >= wordB['start'] and wordA['start'] < wordB['end']):
+    #                 ent1 = wordA['raw_entity_type']
+    #                 ent2 = wordB['raw_entity_type']
+    #                 if priority_dict[ent1] < priority_dict[ent2]:
+    #                     to_remove.add(i)
+    #                 else:
+    #                     to_remove.add(j)
+    # detected_data['entities'] = [ent for idx,ent in enumerate(detected_data['entities']) if idx not in to_remove]
     return detected_data
 
     # ----------------------Entity validation is checked ---------------------------
 def valid_check(boundary_checked_data):
     boundary_checked_data['entities'] = [i for i in boundary_checked_data['entities'] if i['boundary_status'] == "complete" or i['boundary_status'] == "multi-token" ]
+    # overlapping check
+    priority_dict = {
+        "EMAIL_ADDRESS" : 9,
+        "IP_ADDRESS" : 8,
+        "IND_ADHAAR" : 7,
+        "IND_PAN" : 7,
+        "IND_PASSPORT" : 7,
+        "IND_VOTER_ID" : 7,
+        "IND_IFSC" : 7,
+        "IND_UPI_ID" : 7,
+        "IND_DRIVING_LICENSE" : 7,
+        "IND_VEHICLE_REGISTRATION_NUMBER" : 7,
+        "PERSON" : 6,
+        "CREDIT_CARD" : 6,
+        "PHONE_NUMBER" : 4,
+    }
+    to_remove = set()
+    for i, wordA in enumerate(boundary_checked_data['entities']):
+        for j, wordB in enumerate(boundary_checked_data['entities']):
+            if i < j:
+                if (wordA['start'] == wordB['start'] and wordA['end'] == wordB['end']) or (wordB['start'] >= wordA['start'] and wordB['start'] < wordA['end']) or (wordA['start'] >= wordB['start'] and wordA['start'] < wordB['end']):
+                    ent1 = wordA['raw_entity_type']
+                    ent2 = wordB['raw_entity_type']
+                    p1 = priority_dict.get(ent1, 0)
+                    p2 = priority_dict.get(ent2, 0)
+                    if p1 < p2:
+                        to_remove.add(i)
+                    else:
+                        to_remove.add(j)
+    boundary_checked_data['entities'] = [ent for idx,ent in enumerate(boundary_checked_data['entities']) if idx not in to_remove]
     return boundary_checked_data
 
 # load file
-file_content = file_upload('text.txt')
+file_content = file_upload('sample_doc.txt')
 detected_data = detector(file_content)
 pprint(f"Detetced_data : {detected_data}")
+# pprint(detected_data)
 
 boundary_checked_data = boundary_checkor(file_content, detected_data)
 pprint(f"boundary_checked_data : {boundary_checked_data}")
+# pprint(boundary_checked_data)
 
 validated_checked_data = valid_check(boundary_checked_data)
 pprint(f"validated_checked_data : {validated_checked_data}")
+# pprint(validated_checked_data)
+# pprint(len(validated_checked_data['entities']))
+
+
