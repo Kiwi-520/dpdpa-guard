3f1d724469bfe52e3afff67ab6446fdd2db47e67
Disha Holmukhe <dishaholmukhe521@gmail.com>
2026-09-05 08:45:54 +0530

    removed unecessary test code

--- normalizer_layer.py
+++ normalizer_layer.py
@@ -2,52 +2,38 @@ from ingestion_layer import file_upload
 from detector_layer import detector, boundary_checkor, valid_check
 from pprint import pprint
 
+ENTITY_TYPE_MAPPING = {
+    "IN_AADHAAR" :"AADHAAR",
+    "IND_ADHAAR" :"AADHAAR",
+    "IN_PAN" :"PAN",
+    "IND_PAN" :"PAN",
+    "IN_PASSPORT" :"PASSPORT",
+    "IND_PASSPORT" :"PASSPORT",
+    "IN_VOTER" :"VOTER_ID",
+    "IND_VOTER_ID" :"VOTER_ID",
+    "IN_VEHICLE_REGISTRATION" :"VEHICLE_REG",
+    "IND_VEHICLE_REGISTRATION_NUMBER" :"VEHICLE_REG",
+    "IN_GSTIN" :"GSTIN",
+    "IND_GSTIN" :"GSTIN",
+    "IND_UPI_ID" :"UPI",
+    "IND_IFSC" :"IFSC",
+    "IND_DRIVING_LICENSE" :"DRIVING_LICENSE",
+    "IN_DRIVING_LICENSE" :"DRIVING_LICENSE",
+    "EMAIL_ADDRESS" :"EMAIL",
+    "PHONE_NUMBER" :"PHONE",
+    "IP_ADDRESS" :"IP_ADDRESS",
+    "PERSON" :"PERSON",
+    "CREDIT_CARD" :"CREDIT_CARD",
+    "NRP" :"UNKNOWN",
+    "LOCATION" :"LOCATION",
+    "ORGANIZATION" :"UNKNOWN"
+}
 
 def normalizer(validated_checked_data):
-    entity_dicts = {
-        "IN_AADHAAR" :"AADHAAR",
-        "IND_ADHAAR" :"AADHAAR",
-        "IN_PAN" :"PAN",
-        "IND_PAN" :"PAN",
-        "IN_PASSPORT" :"PASSPORT",
-        "IND_PASSPORT" :"PASSPORT",
-        "IN_VOTER" :"VOTER_ID",
-        "IND_VOTER_ID" :"VOTER_ID",
-        "IN_VEHICLE_REGISTRATION" :"VEHICLE_REG",
-        "IND_VEHICLE_REGISTRATION_NUMBER" :"VEHICLE_REG",
-        "IN_GSTIN" :"GSTIN",
-        "IND_GSTIN" :"GSTIN",
-        "IND_UPI_ID" :"UPI",
-        "IND_IFSC" :"IFSC",
-        "IND_DRIVING_LICENSE" :"DRIVING_LICENSE",
-        "IN_DRIVING_LICENSE" :"DRIVING_LICENSE",
-        "EMAIL_ADDRESS" :"EMAIL",
-        "PHONE_NUMBER" :"PHONE",
-        "IP_ADDRESS" :"IP_ADDRESS",
-        "PERSON" :"PERSON",
-        "CREDIT_CARD" :"CREDIT_CARD",
-        "NRP" :"UNKNOWN",
-        "LOCATION" :"LOCATION",
-        "ORGANIZATION" :"UNKNOWN"
-    }
     for i in validated_checked_data['entities']:
-        if i['raw_entity_type'] in entity_dicts:
-            i['entity_type'] = entity_dicts[i['raw_entity_type']]
+        if i['raw_entity_type'] in ENTITY_TYPE_MAPPING:
+            i['entity_type'] = ENTITY_TYPE_MAPPING[i['raw_entity_type']]
         else:
             i['entity_type'] = "UNKNOWN"
 
     return validated_checked_data
-
-
-# load file
-file_content = file_upload('sample_doc.txt')
-
-detected_data = detector(file_content)
-
-boundary_checked_data = boundary_checkor(file_content, detected_data)
-
-validated_checked_data = valid_check(boundary_checked_data)
-
-normalized_data = normalizer(validated_checked_data)
-
-pprint(normalized_data)
